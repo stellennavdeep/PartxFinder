863583ab01ecea3b4db63b48b691d473ce6df141
Navdeep <navdeep@stelleninfotech.in>
2024-12-30 11:55:51 +0530

    sidebar

--- adminpanel/js/script.js
+++ adminpanel/js/script.js
@@ -1,6 +1,6 @@
 const sidebarToggle = document.querySelector("#sidebar-toggle");
 sidebarToggle.addEventListener("click",function(){
-    document.querySelector("#sidebar").classList.toggle("collapsed");
+    document.querySelector("body").classList.toggle("collapsed");
 });
 
 // dropdown
